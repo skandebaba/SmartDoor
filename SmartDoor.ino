745a68e30c33c4cba786210bcce4a47ca5d367eb
Johann Nel <johann.padkos@gmail.com>
2013-08-14 21:16:25 +1200

    Fixed Major Issues
    LED now flashes for different scenarios:
    Solid - Smoke Detected/Smoke Detector Not Working
    On off regularly - Manual Call Point Switch Disconnected
    Long off then On - Press To Exit Switch Disconnected
    Added Interrupt for Smoke Sensor
    Changed Interrupt 0 to RISING edge
    Removed lots of unnecessary code & tidied it up

--- SmartDoor.ino
+++ SmartDoor.ino
@@ -1,20 +1,16 @@
 #include <util/delay.h> 
 
 volatile unsigned char LED [] = {8,9};          // Pin 8 Electromagnetic Lock
-                                      // Pin 9 Sensor 1
-                                      // Pin 10 Sensor 2
+                                                // Pin 9 Warning LED
 unsigned char BUTTON [] = {0,5};                // Button[0] used as manual call point, interrupt 0 is located on Pin 2
-                                      // Pin 5 used as switch to control electromagnetic lock
-                                             
-                                      
-unsigned char Smoke_Sensor = 3;                 // Pin 3 used for Smoke Sensor (MQ-2)  
+                                                // Pin 5 used as switch to control electromagnetic lock                                                                                
+unsigned char Smoke_Sensor = 1;                 // Interrupt 1 located on Pin 3, which is used for Smoke Sensor (MQ-2)      
+boolean Exit_Switch_Operated = false;           // Set Emergency Switch as FALSE (Not Pressed)
 
-// boolean Temp_Sensor = false;          // Set Reading of Sensors as FALSE (No Reading)
-boolean Smoke_Sensor_Operating = false;              
-boolean Sensors_Working = false;          
-boolean Switch_Operated = false;    // Set Emergency Switch as FALSE (Not Pressed)
+// ===========================================================================================
+// Setup Routine - Executes only once when reset it pressed/arduino powered for first time
+// ===========================================================================================
 
-// The Setup Routine executes once when reset is pressed/arduino powered for first time
 void setup(){
   for (int i = 0; i < 2; i++){
     pinMode(BUTTON[i], INPUT);
@@ -23,64 +19,65 @@ void setup(){
   }
   
   pinMode(Smoke_Sensor, INPUT);
-//  digitalWrite(Smoke_Sensor, LOW);
   
-  //Attach the interrupt to the input pin and monitor for ANY Change
-  attachInterrupt(BUTTON[0], override_delay, CHANGE);
+  attachInterrupt(BUTTON[0], override_delay, RISING);          // RISING to trigger when the pin goes from low to high
+  attachInterrupt(Smoke_Sensor, smoke_override_delay, LOW);    // LOW to trigger the interrupt whenever the pin is low
 }
 
-// The Loop Routine executes over and over again
+// ===========================================================================================
+// Main Program - Repeats over and over again
+// ===========================================================================================
+
 void loop(){
-  check_sensors();
   
-  // Reads State of Emergency_Switch
-  Switch_Operated = digitalRead(BUTTON[1]);
+  Exit_Switch_Operated = digitalRead(BUTTON[1]);    
   
-  // If sensors AREN'T working and emergency switch is operated
-  // the electromagnet should unlock instantaneously, else
-  // if the sensors are working and switch is operated delay occurs before unlocking
-  if (Sensors_Working == false){
-    // Checks whether Emergency Switch Operated
-    if (Switch_Operated == LOW){
-      digitalWrite(LED[0], HIGH);
-    }
-    else {
-      digitalWrite(LED[0], LOW);
-      delay_ms(2000);
-    }
-  } else
-  {
-    if (Switch_Operated == LOW){
-      digitalWrite(LED[0], HIGH);
-    }
-    else {
-      delay_ms(3000);
-      digitalWrite(LED[0], LOW);
-      delay_ms(2000);
-    }
-  }
-}
-
-// Execute this code when external interrupt 0 is activated
-void override_delay(){
-  digitalWrite(LED[0],LOW);
-  delay_ms(2000);
-}
-
-void check_sensors(){
-  Smoke_Sensor_Operating = digitalRead(Smoke_Sensor);
-
-  // Checks Whether Sensors are working
-  if (Smoke_Sensor_Operating == true){
-    Sensors_Working = true;
+  if (Exit_Switch_Operated == LOW){
+    digitalWrite(LED[0], LOW);
     digitalWrite(LED[1], LOW);
-  }
-  else if (Smoke_Sensor_Operating == false){
-    Sensors_Working = false;
-    Smoke_Sensor_Operating = false;
-    digitalWrite(LED[1], HIGH);
-  }
+   }
+   else {
+     delay_ms(3000);
+     digitalWrite(LED[0], HIGH);
+     delay_ms(2000);
+     led_flash();
+    }
+    
 }
+
+// ===========================================================================================
+// Interrupt Code
+// ===========================================================================================
+
+// Execute this code when external interrupt 0 is activated i.e. Manual Call Point
+void override_delay(){
+  digitalWrite(LED[0],HIGH);
+  led_flash();
+  delay_ms(1000);
+}
+
+// Execute this code when external interrupt 1 is activated i.e. Smoke Sensor
+void smoke_override_delay(){
+  digitalWrite(LED[0],HIGH);
+  digitalWrite(LED[1], HIGH);
+  delay_ms(1000);
+}
+
+// ===========================================================================================
+// LED Flash Function
+// ===========================================================================================
+
+void led_flash()
+{
+  digitalWrite(LED[1], HIGH);
+  delay_ms(1000);
+  digitalWrite(LED[1], LOW);
+  delay_ms(1000);
+}
+
+// ===========================================================================================
+// Delay Function
+// ===========================================================================================
 
 void delay_ms(unsigned int time) 
 { 
